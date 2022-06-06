8454d2f543840387d2ef6b6c3793231110fe431a
Aditya Biswal <95116291+adityabiswal2147207@users.noreply.github.com>
2022-06-06 10:51:31 +0530

    middleware added

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var express = require('express');
+var expressValidator = require('express-validator');
+var session = require('express-session');
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
+var flash = require('connect-flash');
 var port = 3000;
 
 var app = express();
 
-app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-//app.use(express.static(path.join(__dirname,'public')));
+
+//express session middleware
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//express validator middleware
+
+
+//connect flash middleware
+app.use(flash());
+app.use(function(req,res,next){
+    res.locals.messages = require('express-messages')(req,res);
+    next();
+});
+
+
+
 
 app.get('/',function(req,res){
     res.render('index');
